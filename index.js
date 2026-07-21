6d523297ab859d04e196e55014df06bec0449ffd
Ian A. Matias Rodriguez <90672536+Ian-Matias@users.noreply.github.com>
2026-07-21 08:45:43 -0400

    Port variable

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 
 // Initialize Express app
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Temporary in-memory storage (not used for persistence)
 let title = "";
